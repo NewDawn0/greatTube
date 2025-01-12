fafa9455e17c842dcb20982c1a2802b439883066
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-12 15:38:54 +0100

    Add navigation controls

--- lib/webview.dart
+++ lib/webview.dart
@@ -111,5 +111,35 @@ class BottomBar extends StatelessWidget {
   }
 }
 
+class NavigationControls extends StatelessWidget {
+  const NavigationControls({required this.controller, super.key});
+  final WebViewController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () async {
+            if (await controller.canGoBack()) {
+              await controller.goBack();
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.arrow_forward),
+          onPressed: () async {
+            if (await controller.canGoForward()) {
+              await controller.goForward();
+            }
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.replay),
+          onPressed: () => controller.reload(),
+        ),
+      ],
     );
   }
+}
