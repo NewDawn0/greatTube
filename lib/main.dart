07d656d3dc5fece4cc9bf2f9aa03ef28dab5eaf2
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-01-12 15:33:14 +0100

    Add main

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:greattube/webview.dart';
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
